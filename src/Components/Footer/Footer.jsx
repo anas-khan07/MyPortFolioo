2d667a2484de3a957c1cb20684e705e03188c3e4
AnasKhan <anasofficial888@gmail.com>
2026-05-14 14:23:37 +0530

    changes kiye

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
     <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
          {/* Name / Logo */}
-        <h2 className="text-xl font-semibold text-purple-500">Rohit Kumar</h2>
+        <h2 className="text-xl font-semibold text-purple-500">Anas Khan</h2>
 
         {/* Navigation Links - Responsive */}
         <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
@@ -50,7 +50,7 @@ function Footer() {
         </div>
          {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Rohit Kumar. All rights reserved.
+          © 2026 Anas Khan. All rights reserved.
         </p>
       </div>
        
